ff05ded68450a38fb60fe117c384457c053c18a4
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-08-29 23:38:01 +0530

    Input error fixed

--- app/src/main/java/com/example/lowerorhigher/MainActivity.java
+++ app/src/main/java/com/example/lowerorhigher/MainActivity.java
@@ -14,17 +14,20 @@ public class MainActivity extends AppCompatActivity {
     Random rand = new Random();
     public void guessNumber(View v){
         EditText guessEditText = (EditText) findViewById(R.id.guessEditText);
-        int guess = Integer.parseInt(guessEditText.getText().toString());
 
-        if (guess > compNum){
-            Toast.makeText(MainActivity.this, "Lower!!", Toast.LENGTH_SHORT).show();
-        }
-        else if (guess < compNum){
-            Toast.makeText(MainActivity.this, "Higher!!", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(MainActivity.this, "Guess is Correct\nTry a new play!!", Toast.LENGTH_SHORT).show();
-            compNum = rand.nextInt(20) + 1;
+        if ( guessEditText.getText().toString().isEmpty())
+            Toast.makeText(MainActivity.this, "Enter the number...", Toast.LENGTH_SHORT).show();
+        else {
+            int guess = Integer.parseInt(guessEditText.getText().toString());
+
+            if (guess > compNum) {
+                Toast.makeText(MainActivity.this, "Lower!!", Toast.LENGTH_SHORT).show();
+            } else if (guess < compNum) {
+                Toast.makeText(MainActivity.this, "Higher!!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Guess is Correct\nTry a new play!!", Toast.LENGTH_SHORT).show();
+                compNum = rand.nextInt(20) + 1;
+            }
         }
     }
 
